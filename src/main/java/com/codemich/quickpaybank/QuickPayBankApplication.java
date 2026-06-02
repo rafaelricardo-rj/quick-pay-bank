90296b6632ac03e9e28d6f9e2363ad85d23fb5a1
Rafal Ricardo <rafael_rikardo@yahoo.com.br>
2026-06-01 21:07:24 -0300

    Initial commit

---
+++ src/main/java/com/codemich/quickpaybank/QuickPayBankApplication.java
@@ -0,0 +1,13 @@
+package com.codemich.quickpaybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPayBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickPayBankApplication.class, args);
+    }
+
+}
