749a1c13fd032c3509fe1b1b9c4930bcdb4ef52a
Rafal Ricardo <rafael_rikardo@yahoo.com.br>
2026-06-02 00:51:54 -0300

    feat: initial account structure with entities and DTOs.

---
+++ src/main/java/com/codemich/quickpaybank/account/Account.java
@@ -0,0 +1,40 @@
+package com.codemich.quickpaybank.account;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_account")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal balance;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/codemich/quickpaybank/account/AccountType.java
@@ -0,0 +1,16 @@
+package com.codemich.quickpaybank.account;
+
+public enum AccountType {
+    CHECKING("Conta Corrente"),
+    SAVINGS("Conta Poupança");
+
+    private final String description;
+
+    AccountType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
